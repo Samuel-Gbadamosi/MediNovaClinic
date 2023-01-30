4a06f63e44f740b377589d9b305fedf2e5cf8b76
samuel <damosisamuel@gmail.com>
2023-01-30 12:46:48 +0100

    added new files to git

---
+++ medinova/index.php
@@ -0,0 +1,203 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home</title>
+    <link rel="stylesheet" href="styles.css">
+    <link rel="stylesheet" href="cdn.datatables.net/1.12.1/css/jquery.dataTables.min.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.1.3/dist/css/bootstrap.min.css"
+        integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.css" />
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://cdn.datatables.net/v/dt/dt-1.11.5/datatables.min.js"></script>
+    <!-- bootsrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles.css">
+    <!-- cdn -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
+        integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+<body class="">
+    <!-- header starts here -->
+    <header class="">
+        <!-- header  top starts here -->
+        <div class="header-t container-fluid">
+            <div class="row">
+                <div class="col-10">
+                    <div>
+                        <ul class="d-flex list-unstyled p-2 rightsty">
+                            <li><i class="fa-solid fa-phone pr-1"> </i> +012 345 6789</li>
+                        <li class="pl-2"> | <i class="fa-regular fa-envelope pr-2"> </i>mail@domain.com</li>
+                        </ul>
+                    </div>
+
+                </div>
+                <div class="col-2">
+                    <div>
+                        <ul class="d-flex justify-content-around list-unstyled rightsty p-2">
+                            <li><i class="fa-brands fa-facebook"></i></li>
+                            <li><i class="fa-brands fa-twitter"></i></li>
+                            <li><i class="fa-brands fa-linkedin"></i></li>
+                            <li><i class="fa-brands fa-instagram"></i></li>
+
+
+
+
+                        </ul>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+                <!-- header bottom starts here -->
+                <div class="header-b container-fluid">
+                    <div class="row">
+                        <div class="col-6 ">
+                            <div class="d-flex pl-2 list-unstyled p-2 myown">
+                                <li class="pr-2"><i class="fa fa-clinic-medical "></i></li>
+                                <li>MEDINOVA</li>
+                            </div>
+
+                        </div>
+                        <div class="col-6">
+                            <div class="">
+                                <ul class="d-flex justify-content-around list-unstyled major  pt-2">
+                                    <li>Home</li>
+                                    <li>About</li>
+                                    <li>Service</li>
+                                    <li>Pricing</li>
+                                    <li>Contact</li>
+                                </ul>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+                <!-- header bottom ends here -->
+
+        
+
+    </header>
+    <!-- header ends here -->
+    <!-- main starts here -->
+    <main class="container-fluids">
+        <section class="sect21">
+            <div class="jumbotron">
+                <div class="row">
+                    <div class=" col-7 myjum">
+                        <h3 >WELCOME TO MEDINOVA</h3>
+                        <h5></h5>
+
+                        <h1>Best Healthcare Solution in Your City</h1>
+
+                        <div>
+                            <button class="btn-lg btn-dark mybt">Find Doctor</button>
+                            <button class="btn-lg btn-dark ml-2 mybt2">Appointment</button>
+                        </div>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+        <section class="sect22 container">
+            <div class="row">
+                <div class="col-5">
+                    <div class="mt-4" >
+                        <img class="imgj" src="surg.jpeg" alt="">
+                    </div>
+
+                </div>
+                <div class="col-7">
+                    <div class="row mt-4 ">
+                        <div class="mjko">
+                            <h3>ABOUT US</h3>
+                            <h5></h5>
+
+
+                            <h1>Best Medical Care for Yourself and Your Family</h1>
+
+                            <p>Tempor erat elitr at rebum at at clita aliquyam consetetur. Diam dolor diam ipsum et, tempor voluptua sit consetetur sit. Aliquyam diam amet diam et eos sadipscing labore. Clita erat ipsum et lorem et sit, sed stet no labore lorem sit. Sanctus clita duo justo et tempor consetetur takimata eirmod, dolores takimata consetetur invidunt magna dolores aliquyam dolores dolore. Amet erat amet et magna</p>
+
+                        </div>
+                        <div class="row pt-4 ">
+                            <div class="col-3">
+                                <div class="circleuno ">
+                                    <ul class="text-center list-unstyled pt-3">
+                                        <li><i class="fa-solid fa-3x fa-user-doctor pricol"></i></li>
+                                        <li class="qu">Qualified</li>
+                                        <li class="res">Doctors</li>
+                                    </ul>
+
+                                </div>
+
+                            </div>
+                            <div class="col-3">
+                                <div class="circleuno">
+                                    <ul class="text-center list-unstyled pt-3">
+                                        <li><i class="fa-solid fa-3x fa-procedures pricol"></i></li>
+                                        <li class="qu">Emergency</li>
+                                        <li class="res">Services</li>
+
+                                    </ul>
+
+                                </div>
+
+                            </div>
+                            <div class="col-3">
+                                <div class="circleuno">
+                                    <ul class="text-center list-unstyled pt-3">
+                                        <li><i class="fa-solid fa-3x fa-microscope pricol"></i></li>
+                                        <li class="qu">Emergency</li>
+                                        <li class="res">Services</li>
+
+                                    </ul>
+
+                                </div>
+
+                            </div>
+                            <div class="col-3">
+                                <div class="circleuno">
+                                    <ul class="text-center list-unstyled pt-3">
+                                        <li><i class="fa-solid fa-3x fa microscope pricol"></i></li>
+                                        <li class="qu">Emergency</li>
+                                        <li class="res">Stector</li>
+
+                                    </ul>
+
+                                </div>
+
+                            </div>
+
+                        </div>
+                        
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </section>
+
+    </main>
+        <!-- main starts here -->
+<!-- footer starts here -->
+    <footer>
+
+    </footer>
+    <!-- footer starts here -->
+
+    
+</body>
+</html>
